17fc08bcd27dc5097be9a830a8a10395ab4ba2af
sirjit2 <sirjeetdahal91@gmail.com>
2026-04-25 23:16:41 +0545

    changed the ui of feedback

--- pages/patient/dashboard.js
+++ pages/patient/dashboard.js
@@ -223,7 +223,7 @@ function renderAppointments(rows) {
             View Feedback
           </button>`
               : item.status_key === "completed" && parseInt(item.rating || 0) === 0
-              ? `<button type="button" data-feedback="${item.appointment_id}" class="feedback-btn w-full px-3 py-2 rounded-lg bg-yellow-400 text-white hover:bg-yellow-500 text-xs font-medium shadow-sm transition whitespace-nowrap text-center">
+              ? `<button type="button" data-feedback="${item.appointment_id}" class="feedback-btn w-full px-3 py-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 text-xs font-medium shadow-sm transition whitespace-nowrap text-center">
             Feedback
           </button>`
               : ""
